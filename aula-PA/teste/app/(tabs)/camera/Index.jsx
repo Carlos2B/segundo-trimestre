38242f44d1fcd2d7a6e6add5b465132d00ff1a12
CARLOS CESAR JUNIOR DOS SANTOS <carlos_cj_santos@estudante.sesisenai.org.br>
2024-10-07 10:21:08 -0300

    atualização07/10

---
+++ aula-PA/teste/app/(tabs)/camera/Index.jsx
@@ -0,0 +1,78 @@
+import { useState, useRef } from "react";
+import { View, StyleSheet, Text, Image, Button } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import * as MediaLibrary from "expo-media-library";
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+
+    textperm: {
+        textAlign: 'center'
+    },
+
+    camera: {
+        flex: 1
+    },
+
+    foto: {
+width: 650,
+height: 650
+    }
+})
+
+export default function camera() {
+
+    const [permissao, pedirPermissao] = useCameraPermissions()
+    const [foto, setFoto] = useState(null)
+    const cameraRef = useRef(null)
+    const [lado, setLado] = useState('back')
+
+
+    if (!permissao) {
+        return <View></View>
+    }
+
+
+    if (!permissao.granted) {
+        return <View style={style.container}>
+            <Text style={style.textperm}>Você aceita que a camera seja usada??</Text>
+            <Button title="Aceitar permissão" onPress={pedirPermissao} />
+        </View>
+    }
+
+    const tirarFoto = async () => {
+        const fotoBase64 = await cameraRef.current?.takePictureAsync({
+            quality: 1,
+            base64: true
+        })
+        setFoto(fotoBase64)
+    }
+
+    const trocaCamera = () => {
+        setLado(lado == 'back' ? 'front' : 'back')
+    }
+
+    const salvarFoto = () => {
+        MediaLibrary.saveToLibraryAsync(foto.uri);
+        setFoto(null);
+    };
+
+    return (
+        <View style={style.container}>
+            {foto ?
+                <View>
+                    <Image source={{ uri: foto.uri }} style={style.foto} />
+                    <Button onPress={() => setFoto(null)} title="Limpar foto" />
+                    <Button onPress={salvarFoto} title="Salvar foto" />
+                </View> :
+                <CameraView facing={lado} style={style.camera} ref={cameraRef}>
+                    <Button title="Tirar foto" onPress={tirarFoto} />
+                    <Button title="Troca de camera" onPress={trocaCamera} />
+                </CameraView>
+            }
+        </View>
+    )
+}
